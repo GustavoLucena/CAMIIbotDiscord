727237625212b540fade91b6eefc6a720fab92a9
DragonYorozuya <dragon6_@hotmail.com>
2019-08-27 21:14:37 -0300

    start

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var url = "https://kamiii.000webhostapp.com/API/anime/listaAnimesEp.php";
 
 //Logando no bot
 const bot = new Discord.Client();
-bot.login('Mzc2MTA4MjM2MDk4NzY0ODAw.XWXEQw.yz27kR4o0iKb3iUGBiM999xDVvI');
+bot.login('Mzc2MTA4MjM2MDk4NzY0ODAw.XWXHTg.44cT4mu-UB-h3r9bQwEC2Th_jIM');
 
 //quando ficar pronto
 bot.once('ready',() =>{ console.log(`online ${bot.user.tag}`) });
